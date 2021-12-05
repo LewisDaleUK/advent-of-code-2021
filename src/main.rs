1744e618a3e649dccc97252d4e13defb43a82f03
Lewis Dale <lewis.dale@fortacode.co.uk>
2021-12-05 21:13:16 +0000

    Day 2, part 1

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,35 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 mod depth;
+mod position;
 
 fn main() {
     let depths = read_depths();
     let times_increased = depth::times_depth_increased(depths);
     println!("{}", times_increased);
+
+    let instructions = read_directions();
+    let (horizontal, depth) = position::calculate_position(instructions, 0, 0);
+    println!("{} final position", horizontal * depth);
+}
+
+fn read_directions() -> Vec<(String, i32)> {
+    if let Ok(lines) = read_lines("./files/day_2_input.txt") {
+        let mut directions: Vec<(String, i32)> = Vec::new();
+
+        for line in lines {
+            if let Ok(l) = line {
+                let mut split: std::str::Split<&str> = l.split(" ");
+                let dir = split.next().unwrap();
+                let amt = split.next().unwrap().parse::<i32>().unwrap();
+
+                directions.push((String::from(dir), amt));
+            }
+        }
+        return directions;
+    } else {
+        return vec![];
+    }
 }
 
 fn read_depths() -> Vec<i32> {
---
+++ src/position.rs
@@ -0,0 +1,36 @@
+pub fn calculate_position(instructions: Vec<(String, i32)>, mut horizontal: i32, mut depth: i32) -> (i32, i32) {
+    if instructions.len() == 0 {
+        return (horizontal, depth);
+    }
+
+    let (direction, amount) = &instructions[0];
+
+    match direction.as_str() {
+        "forward" => horizontal += amount,
+        "down" => depth += amount,
+        "up" => depth -= amount,
+        _ => horizontal = horizontal
+    };
+
+    return calculate_position(Vec::from(&instructions[1..]), horizontal, depth);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test] 
+    fn calculates_depth_and_horizontal_position() {
+        let instructions: Vec<(String, i32)> = vec![
+            (String::from("forward"), 5),
+            (String::from("down"), 5),
+            (String::from("forward"), 8),
+            (String::from("up"), 3),
+            (String::from("down"), 8),
+            (String::from("forward"), 2)
+        ];
+
+        let (horizontal, depth) = super::calculate_position(instructions, 0, 0);
+
+        assert_eq!(horizontal, 15);
+        assert_eq!(depth, 10);
+    }
+}
